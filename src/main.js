77a58db7f2c1c15c43b878f90c069fc4cd68cc13
Ic Lepitar <1585576796@qq.com>
2020-08-10 14:59:00 +0800

    init

--- src/main.js
+++ src/main.js
@@ -2,11 +2,22 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+import axios from 'axios'
 
 Vue.config.productionTip = false
+
+Vue.prototype.$http = axios
+
+axios.defaults.baseURL = 'http://localhost:18888'
+axios.defaults.timeout = 5000
+axios.defaults.headers.post['Content-Type'] = 'application/json;charset=UTF-8'
+
+Vue.use(ElementUI)
 
 new Vue({
   router,
   store,
-  render: h => h(App)
+  render: (h) => h(App)
 }).$mount('#app')
