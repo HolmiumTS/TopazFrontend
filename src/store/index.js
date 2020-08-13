e2c7272253c7d51b2aaf1fd9c51ac8eb3d863024
Ic Lepitar <1585576796@qq.com>
2020-08-13 14:25:39 +0800

    use sessionstorage

--- src/store/index.js
+++ src/store/index.js
@@ -7,11 +7,11 @@ const store = new Vuex.Store({
   //用status表示登录状态:-1为未登录,0为已登录主界面,1为团队界面
   state: {
     // status: '0', // debug
-    status: '0',
-    userId: '',
-    username: '',
-    avatar: '',
-    teamId: '',
+    status: '-1' || JSON.parse(sessionStorage.getItem('state')).status,
+    userId: '' || JSON.parse(sessionStorage.getItem('state')).userId,
+    username: '' || JSON.parse(sessionStorage.getItem('state')).username,
+    avatar: '' || JSON.parse(sessionStorage.getItem('state')).avatar,
+    teamId: '' || JSON.parse(sessionStorage.getItem('state')).teamId,
   },
   mutations: {
     Login(state, value) {
@@ -20,19 +20,23 @@ const store = new Vuex.Store({
       state.userId = value.userId;
       state.username = value.username;
       state.avatar = value.avatar;
+      sessionStorage.setItem("state", JSON.stringify(state));
     },
     Logout(state) {
       state.status = '-1';
       state.userId = state.username = state.avatar = '';
+      sessionStorage.removeItem("state");
     },
     ChangeStatus(state, status) {
       console.log(status);
       state.status = status;
+      sessionStorage.setItem("state", JSON.stringify(state));
       console.log(state.status);
     },
     ChangeTeamId(state, teamId) {
       console.log(teamId);
       state.teamId = teamId;
+      sessionStorage.setItem("state", JSON.stringify(state));
       console.log(state.teamId);
     },
   },
